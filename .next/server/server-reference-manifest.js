a2dc52c0dc15e8307330017f8a3027e3a98f4c91
Ava Collins <info@avacollins.dev>
2025-04-10 10:43:25 -0400

    chore:react app

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ChePLEq2Tmek4c4LCP/o9sTpWM6VUnc9+vmzvw9r8nI=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <main className="main">
+        <section>
+          <h1>hello world</h1>
+          <p>Lorem ipsum I wish i knew more but I dont.</p>
+        </section>
+      </main>
+    </React.Fragment >
+  );
+}
